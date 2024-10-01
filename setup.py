1d89a001dbc785ef431bcc4dca28ac01a2eaff47
kris.wang <wenhom.wang@gmail.com.com>
2024-10-01 21:49:31 +0800

    update setup.py and README

--- setup.py
+++ setup.py
@@ -11,23 +11,28 @@ def import_module(file: str):
         sys.path.append(str(file.parent))
     return importlib.import_module(file.stem)
 
+path = Path(__file__).parent.resolve()
 
-package_path = Path(__file__).resolve().parent / r"rangeutils"
+readme_file = path / "README.md"
+package_path = path / r"rangeutils"
 version = import_module(file=package_path / "version.py")
-
-long_description = ""
-with open("README.md") as f:
-    long_description = f.read()
 
 setup(
     name="rangeutils",
     version=version.__version__,
+    description="Utilities for manipulating and converting Python ranges and boolean lists",
+    long_description=readme_file.read_text(encoding="utf8"),
+    long_description_content_type="text/markdown",
+    license="MIT",
+
+    author="Kris Wang",
+    author_email="wenhom.wang@gmail.com",
+    url="https://github.com/Hom-Wang/rangeutils",
+
     packages=find_packages(),
     package_data={"rangeutils": ["*"]},
     # include_package_data=True,
     entry_points={"console_scripts": []},
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     python_requires=">=3.10",
     install_requires=[
         "numpy",
